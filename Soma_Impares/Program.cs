e3dd3b8930197dc791e96c08cf5b5ac89fc462d8
Kappy <115071945+KappyStefani@users.noreply.github.com>
2023-08-30 08:45:31 -0300

    Add files via upload

---
+++ Soma_Impares/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma, troca;
+
+            Console.WriteLine("Digite dois números: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if(x > y)
+            {
+                troca = x;
+                x = y;
+                y = troca;
+            }
+
+            soma = 0;
+
+            for(int i = x+1; i < y; i++) 
+            {
+                if(i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine("Soma dos impares: " + soma);
+
+        }
+    }
+}
