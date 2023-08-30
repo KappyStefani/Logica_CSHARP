826df5a12f7eb08b516a7a44d5bd904eef493907
Kappy <115071945+KappyStefani@users.noreply.github.com>
2023-08-30 08:31:44 -0300

    Add files via upload

---
+++ Menor_De_Tres/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n1, n2, n3;
+
+            Console.WriteLine("Primeiro número");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo número");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Terceiro número");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if(n1 < n2 && n1 < n3)
+            {
+                Console.WriteLine("O menor número é: " + n1);
+                    
+            }
+            else
+            {
+                if(n2 < n3 && n2 < n1)
+                {
+                    Console.WriteLine("O menor número é " + n2);
+                }
+                else 
+                {
+                    Console.WriteLine("O menor número é " + n3);
+                }
+            }
+            
+
+        }
+    }
+}
