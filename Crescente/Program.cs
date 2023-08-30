4d039257360d917347bbc23480cc58871c79745f
Kappy <115071945+KappyStefani@users.noreply.github.com>
2023-08-30 08:31:06 -0300

    Add files via upload

---
+++ Crescente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Digite dois números: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            while (n1 != n2)
+            {  
+
+                if (n1 < n2)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                Console.WriteLine("Digite outros dois números: ");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                n2 = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
