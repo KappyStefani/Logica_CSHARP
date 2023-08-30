0f4007557b2d331cf323f69d7f744716e66267fa
Kappy <115071945+KappyStefani@users.noreply.github.com>
2023-08-30 05:46:49 -0300

    Add files via upload

---
+++ Idades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2;
+            int idade1, idade2;
+            double media;
+            
+            Console.WriteLine("Dados da primeira pessoa: ");
+            
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade2 = Convert.ToInt32(Console.ReadLine());
+
+            media = Convert.ToDouble((idade1) + idade2) / 2;
+
+            Console.WriteLine("A media de idade de " + nome1 + " e " + nome2 +  " é de: " + media );
+        }
+    }
+}
