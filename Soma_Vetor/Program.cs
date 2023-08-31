c886b0e16c8e066f4030f5f0e06b0d644cdbe04f
Kappy <115071945+KappyStefani@users.noreply.github.com>
2023-08-31 02:14:08 -0300

    Add files via upload

---
+++ Soma_Vetor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double soma, media;
+
+            Console.Write("Quantos números você vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um número: ");
+                vet[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("Números digitados: ");
+            
+
+            for(int i = 0; i < N; ++i) 
+            {
+                Console.Write(vet[i] + " ");               
+            }
+
+            soma = 0;
+
+            for(int i = 0; i < N; i++)
+            {
+                soma = soma + vet[i];
+            }
+
+            Console.WriteLine("\nSoma: " + soma);
+
+            media = (double)soma / N;
+
+            Console.WriteLine("Media: " + media);
+
+
+        }
+    }
+}
